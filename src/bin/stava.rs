bfc7606842959841974c13db34918e09b3d3dd82
Simon Egersand <segersand@spotify.com>
2022-03-08 22:15:29 +0100

    Bump dependencies and code fixes

--- src/bin/stava.rs
+++ src/bin/stava.rs
@@ -4,12 +4,12 @@ extern crate clap;
 #[macro_use]
 extern crate include_dir;
 
-use clap::{App, Arg};
+use clap::{Arg, Command};
 use include_dir::Dir;
 use stava::{Stava, StavaResult};
 
 use std::collections::HashMap;
-use std::ffi::{OsStr, OsString};
+use std::ffi::OsStr;
 use std::fs;
 use std::path::Path;
 use std::process::exit;
@@ -23,34 +23,35 @@ const FLAG_ONLY_EXIT_CODE: &str = "flag_only_exit_code";
 const ASSETS_DIR: Dir = include_dir!("src/assets");
 
 fn main() {
-    let opt_word = Arg::with_name(OPT_NAME_WORD)
+    let opt_word = Arg::new(OPT_NAME_WORD)
         .help("Word to correct")
         .required(true)
         .index(1);
 
-    let opt_files = Arg::with_name(OPT_NAME_FILES)
+    let opt_files = Arg::new(OPT_NAME_FILES)
         .help("Files to learn words from")
-        .multiple(true)
+        .takes_value(true)
+        .multiple_values(true)
         .required(false)
         .validator_os(exists_on_filesystem)
         .index(2);
 
-    let flag_inc_default_words = Arg::with_name(FLAG_INC_DEFAULT_WORDS)
+    let flag_inc_default_words = Arg::new(FLAG_INC_DEFAULT_WORDS)
         .help("Include default set of words (default: false)")
-        .short("d")
+        .short('d')
         .long("default");
 
-    let flag_return_exit_code = Arg::with_name(FLAG_RETURN_EXIT_CODE)
+    let flag_return_exit_code = Arg::new(FLAG_RETURN_EXIT_CODE)
         .help("Exit with 1 if word is not spelled correctly, otherwise 0 (default: false)")
-        .short("e")
+        .short('e')
         .long("exit-code");
 
-    let flag_only_exit_code = Arg::with_name(FLAG_ONLY_EXIT_CODE)
+    let flag_only_exit_code = Arg::new(FLAG_ONLY_EXIT_CODE)
         .help("Only return exit code and not corrected word (default: false)")
-        .short("o")
+        .short('o')
         .long("exit-code-only");
 
-    let matches = App::new("stava")
+    let matches = Command::new("stava")
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
@@ -111,9 +112,9 @@ fn get_default_words() -> &'static str {
         .unwrap_or_else(|| panic!("Could not get default words"))
 }
 
-fn exists_on_filesystem(path: &OsStr) -> Result<(), OsString> {
+fn exists_on_filesystem(path: &OsStr) -> Result<(), String> {
     match Some(path).map(Path::new).map(Path::exists).unwrap_or(false) {
         true => Ok(()),
-        false => Err(OsString::from(format!("File not found [{:?}]", path))),
+        false => Err(format!("File not found [{:?}]", path)),
     }
 }
